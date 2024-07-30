01e4b682064b854f76662bb8ee92223da5d5bcfb
leehj112 <159211698+leehj112@users.noreply.github.com>
2024-07-30 10:52:52 +0900

    Add files via upload

---
+++ 자료구조 알고리즘/ch3/ssearch_test2.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 30 10:51:49 2024
+
+@author: leehj
+"""
+
+# [Do it! 실습 3C-2] seq_search() 함수를 사용하여 특정 인덱스 검색하기
+
+from ssearch_while import seq_search
+
+t = (4, 7, 5.6, 2, 3.14, 1)
+s = 'string'
+a = ['DTS', 'AAC', 'FLAC']
+
+print(f'{t}에서 5.6의 인덱스는 {seq_search(t, 5.6)}입니다.')
+print(f'{s}에서 "n"의 인덱스는 {seq_search(s, "n")}입니다.')
+print(f'{a}에서 "DTS"의 인덱스는 {seq_search(a, "DTS")}입니다.')
