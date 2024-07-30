8d5b9d9408faa458d7713804f0567cf454990a0c
leehj112 <159211698+leehj112@users.noreply.github.com>
2024-07-30 11:07:31 +0900

    Add files via upload

---
+++ 자료구조 알고리즘/ch5/recur1a.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 30 11:05:48 2024
+
+@author: leehj
+"""
+
+# [Do it! 실습 5-4] 재귀 함수의 구현(꼬리 재귀를 제거)
+
+def recur(n: int) -> int:
+    """꼬리 재귀를 제거한 함수 recur"""
+    while n > 0:
+        recur(n - 1)
+        print(n)
+        n = n - 2
+x = int(input('정수값을 입력하세요.: '))
+
+recur(x)
---
+++ 자료구조 알고리즘/ch5/recur2.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 30 11:06:20 2024
+
+@author: leehj
+"""
+
+# recur 함수를 거꾸로 출력(Do it! 실습 5-3 수정)
+
+def recur(n: int) -> int:
+    """순수한 재귀 함수 recur의 구현(거꾸로 출력)"""
+    if n > 0:
+        recur(n - 2)
+        print(n)
+        recur(n - 1)
+
+x = int(input('정숫값을 입력하세요.: '))
+
+recur(x)
