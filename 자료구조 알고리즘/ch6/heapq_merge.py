5db17a5f7a3cf7598ce1c3c6325a8d25d5d4eade
leehj112 <159211698+leehj112@users.noreply.github.com>
2024-07-30 11:35:35 +0900

    Add files via upload

---
+++ 자료구조 알고리즘/ch6/heapq_merge.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 30 11:26:23 2024
+
+@author: leehj
+"""
+
+# 정렬을 마친 두 배열의 병합 (heapq.merege 사용）
+
+import heapq
+
+a = [2, 1, 6, 8, 11, 13]
+b = [1, 2, 3, 4, 9, 16, 21]
+c = list(heapq.merge(a, b))  # 배열 a와 b를 병합하여 c에 저장
+
+print('배열 a와 b를 병합하여 배열 c에 저장하였습니다.')
+print(f'배열 a: {a}')
+print(f'배열 b: {b}')
+print(f'배열 c: {c}')
